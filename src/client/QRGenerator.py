3492bda3cc7218548ede13e50d707e8a6b4078f4
MadeAdhika39 <18220003@std.stei.itb.ac.id>
2022-11-23 19:29:10 +0700

    Update QRGenerator.py
    Dah ditengah ya

--- src/client/QRGenerator.py
+++ src/client/QRGenerator.py
@@ -50,12 +50,13 @@ class Image(qrcode.image.base.BaseImage):
  
         # assigning border
         self.border = border
- 
+
         # assigning  width
         self.width = width
  
         # assigning box size
         self.box_size = box_size
+
  
         # creating size
         size = (width + border * 2) * box_size
@@ -97,7 +98,7 @@ class QRWindow(QMainWindow):
         # setting window title
         self.setFixedSize(1280, 720)
         self.setWindowTitle("QR Code")
-        self.setContentsMargins(100,100,0,200)
+        self.setContentsMargins(100,-100,100,200)
         # setting geometry
         #self.setGeometry(100, 100, 300, 300)
         self.setStyleSheet(f'background-color: {DARK_MODE_BG}')
