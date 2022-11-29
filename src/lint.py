1b20d47895aa3ac8c5ab80415135da0e3533a16a
umrhkm <umarhakim125@gmail.com>
2022-11-29 12:06:58 +0700

    Create lint.py

---
+++ src/lint.py
@@ -0,0 +1,18 @@
+import sys 
+
+from pylint import lint  
+
+THRESHOLD = 9  
+
+run = lint.Run(["app.py"], do_exit=False) 
+
+score = run.linter.stats["global_note"]  
+
+if score < THRESHOLD: 
+
+    print("Linter failed: Score < threshold value") 
+
+    sys.exit(1) 
+
+
+sys.exit(0) 
